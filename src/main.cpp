76544ee4e121d22cf0bc373690f39331107cabff
jon3er <jonathandreyer14@gmail.com>
2024-12-15 22:40:33 +0100

    Changes main fkt

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,14 @@
 #include <uart_com.h>
 #include <uart_test.h>
 #include <e_uart_msg.h>
+#include <LiquidCrystal.h>
+#include <string.h>
+// initialize the library by associating any needed LCD interface pin
+
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup(){
 
@@ -10,9 +18,12 @@ void setup(){
   Serial.begin(250000); //initialize serial communication at a 250000 baud rate
   Serial1.begin(250000);
   // Display setup
+    // set up the LCD's number of columns and rows:
+
+  lcd.begin(16, 2);
+
 
 }
-
 
 void loop(){
   
@@ -20,17 +31,37 @@ void loop(){
   //uart_passthrough();
    //uart_test();
 
-  int BitNumb;
-  int MsgNumb;
-  bool ComAktiv;
-  int ComStep;
-  int ComStatus;
-  int TimeOutCnt;
-  int MaxTimeOut= 50000000;
+// Consitendte werte müssen auserhalb der loop gespeichert werden
+int BitNumb;
+int MsgNumb;
+bool ComAktiv;
+bool LcdPrint;
+int ComStep;
+int ComStatus ;
+int TimeOutCnt;
+int MaxTimeOut;
+int VarBuffer;
+int CoolDown;
+
+bool startup;
+
+// set Init values
+if (!startup)
+{
+  startup = true;
+  LcdPrint = false;
+  BitNumb;
+  MsgNumb = 0;
+  ComAktiv = false;
+  ComStep = 0;
+  ComStatus = 0;
+  TimeOutCnt = 0;
+  MaxTimeOut= 5000;
+}
 
 
-    //set Var for startup
-  int DataInStart[msgtype::numberStart][msgtype::MaxLgthStrt] ={
+  //set Var for startup
+int DataInStart[msgtype::numberStart][msgtype::MaxLgthStrt] ={
     //msg 1
     //{85 ,32 ,240,},
     {0x55,0x20,0xf0},
@@ -44,21 +75,18 @@ void loop(){
     //{85, 129, 208, 7, 0, 0, 200, 0, 0, 0, 1, 54, 16, 1, 192, 100, 0, 0, 0, 30, 30, 16, 39, 0, 0, 0, 200, 0, 48, 117, 4, 0, 0, 0}
     {0x55,0x81,0xD0,0x07,0x00,0x00,0xC8,0x00,0x00,0x00,0x01,0x36,0x10,0x01,0xC0,0x64,0x00,0x00,0x00,0x1E,0x1E,0x10,0x27,0x00,0x00,0x00,0xC8,0x00,0x30,0x75,0x04,0x00,0x00,0x00},
   };
-
-
-  int DataOutStart[msgtype::numberStart][msgtype::MaxLgthStrt];
-
-  //set Var for Communication
-  int DataInCom[msgtype::numberCom][msgtype::MaxLgthCom]={
+int DataOutStart[msgtype::numberStart][msgtype::MaxLgthStrt];
+//set Var for Communication
+int DataInCom[][msgtype::MaxLgthCom]={
     //Com msg 1
     //{85, 72, 25, 4, 23, 4, 239, 55, 239, 55,   4,  42,   69,  0,  0, 2, 0, 0, 0, 0},
     //                                                    ____ <- winkel              
-    {0x55,0x48,0x19,0x04,0x17,0x04,0xEF,0x37,0xEF,0x37,0x04,0x31,0x00,0x00,0x00,0x02,0x00,0x00,0x00,0x00},
+    {0x55,0x48,0x19,0x04,0x17,0x04,0xEF,0x37,0xEF,0x37,0x04,0x31,0x40,0x00,0x00,0x02,0x00,0x00,0x00,0x00},
     //Com msg 2
     //{85, 89, 33, 2,  0, 0,  83, 49,   0, 68, 224, 167, 255, 68, 76, 0, 0, 0, 0}
     {0x55,0x59,0x21,0x02,0x00,0x00,0x53,0x31,0x00,0x44,0xE0,0xA7,0xFF,0x44,0x4C,0x00,0x00,0x00,0x00}
   };
-
+int DataOutCom[msgtype::numberCom][msgtype::MaxLgthCom];
 
 /*
 Startup_com(ComAktiv);
@@ -74,30 +102,43 @@ else if (!ComAktiv)
 }
 */
 
-if (ComStep == 0)
+if(LcdPrint || startup)
 {
-  Serial.write(0x99);
+// Wert des zündwinkels ausgeben bit 13
+VarBuffer=DataInCom[0][12];
+lcd.setCursor(0, 0);
+lcd.print("ZW:");
+lcd.print(VarBuffer);
 
+// Wert der Frequenz ausgeben bit 13
+VarBuffer=DataInCom[0][11];
+lcd.setCursor(6, 0);
+lcd.print("Freq:");
+lcd.print(VarBuffer);
+
+// Debug Print
+// Wert der Frequenz ausgeben bit 13
+VarBuffer=ComStep;
+lcd.setCursor(0, 1);
+lcd.print("setp:");
+lcd.print(VarBuffer);
+
+// Wert der Frequenz ausgeben bit 13
+VarBuffer=ComStatus;
+lcd.setCursor(9, 1);
+lcd.print("Stat:");
+lcd.print(VarBuffer);
 }
-else if (ComStep == 1)
-{
-  Serial.write(0x44);
-}
-else
-{
-  Serial.write(0x11);
-}
- 
+//test serial write
 
 
-  int DataOutCom[msgtype::numberCom][msgtype::MaxLgthCom];
-
-
+// Com Start
   switch (ComStep)
   {
   case 0:
 
-    if (combyte::Startbyte == Serial.read() & !ComAktiv)
+    //if ((combyte::Startbyte == Serial.read()) && !ComAktiv)
+    if (true)
     {
 				ComAktiv = true;
     }
@@ -107,7 +148,7 @@ else
       ComStep = 1;
 
     };
-
+    Serial.print(0x01);
     break;
   case 1:
     ComStatus = Uart_Start_Com(ComAktiv, BitNumb, MsgNumb, DataInStart[msgtype::numberStart][msgtype::MaxLgthStrt], DataOutStart[msgtype::numberStart][msgtype::MaxLgthStrt]);
@@ -116,16 +157,30 @@ else
     {
       ComStep= 99;
     };
-
+    Serial.print(0x02);
     break;
   default:
     
-    Uart_Data_Com(ComAktiv, BitNumb, MsgNumb, DataInCom[msgtype::numberCom][msgtype::MaxLgthCom], DataOutCom[msgtype::numberCom][msgtype::MaxLgthCom]);
-
+    ComStatus = Uart_Data_Com(ComAktiv, BitNumb, MsgNumb, DataInCom[msgtype::numberCom][msgtype::MaxLgthCom], DataOutCom[msgtype::numberCom][msgtype::MaxLgthCom]);
+    Serial.print(0x03);
     break;
   }
   
- //
+  // wenn Com pause und cooldown zuende  lcdPrint
+  if ((ComStatus == 1) && (CoolDown == 0))
+  {
+    LcdPrint = true;
+    CoolDown = 1000;
+  }
+  else if (ComStatus == 1)
+  {
+    LcdPrint = false;
+    CoolDown--;
+  }
+  
+
+
+ // Time out
  // ComAktiv = uart_timeout(TimeOutCnt, MaxTimeOut);
  //
  // if (!ComAktiv)
